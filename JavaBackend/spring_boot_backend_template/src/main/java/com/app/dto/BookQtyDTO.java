68150f013d95a8ba781aed288f5def6fdd606338
KunalP1303 <kunalpatle.kp@gmail.com>
2024-02-09 21:49:52 +0530

    BookQty DTO Added

---
+++ JavaBackend/spring_boot_backend_template/src/main/java/com/app/dto/BookQtyDTO.java
@@ -0,0 +1,19 @@
+package com.app.dto;
+
+import com.app.entities.Book;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookQtyDTO {
+
+	private Book book;
+	
+	private int quantity;
+}
