5500af0a1da769c7e83984e4ce8818f4d55b55fa
D3-80669-Apoorva Vairagi <codesaliecc@gmail.com>
2024-02-09 14:44:40 +0530

    Navbar 2 added

--- React_project/00demo/src/Components/Navbar2.jsx
+++ React_project/00demo/src/Components/Navbar2.jsx
@@ -1,99 +1,40 @@
-import "./Styles/componentStyles.css";
-import Home from "./Pages/Home";
-import {Link, Switch, Route} from 'react-router-dom';
-function MyNavbar2() {
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function Navbar2() {
   return (
-    <>
-      <nav class="navbar navbar-expand-lg navbar-light navbar2">
-        <div class="container-fluid">
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Geners
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                    <a class="dropdown-item" href="#fanatsy">
-                      Fantasy
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Fiction
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Non Fiction
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#selfhelp">
-                      Self-Help
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Horror
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#bookset">
-                      Book-Set
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#manga">
-                      Manga
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                {/* <a class="nav-link active" aria-current="page" href="#">
-                  Home</Link>
-                </a> */}
-                <Link to="/">Home</Link>
-                {/* <Route path="/" component={Home} exact /> */}
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                 
-                </a>
-              </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link disabled"
-                  href="#"
-                  tabindex="-1"
-                  aria-disabled="true"
-                >
-                  Disabled
-                </a>
-              </li>
-              <li class="nav-item">
-                <Link to="/cart">Cart</Link>
-              </li>
-              <li class="nav-item">
-          <Link to="/wishlist">Wishlist</Link>
-              </li>
-              <li class="nav-item">
-                <Link to="/orders">Orders</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+          <NavDropdown title="Geners" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Fantasy</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Fiction
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Non-Fiction</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">
+                Self-Help
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.5">
+                Horror
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.6">
+                Book-Set
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Cart</Nav.Link>
+            <Nav.Link href="#link">Wish-List</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default MyNavbar2;
+export default Navbar2;
