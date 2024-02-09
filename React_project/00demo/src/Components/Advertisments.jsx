1e2c5fae79df33cdbb52bd6ce7a0fc0447859a17
D3-80669-Apoorva Vairagi <codesaliecc@gmail.com>
2024-02-09 16:48:40 +0530

    Advertisment Component Added

--- React_project/00demo/src/Components/Advertisments.jsx
+++ React_project/00demo/src/Components/Advertisments.jsx
@@ -1,38 +1,21 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import imageLibrary from '../adsImagesArr';
+import '../Components/Styles/adsImagesStyle.css';
+
 function Advertisments() {
   return (
-    <>
-      <div id="carouselExampleInterval10" className="carousel slide" data-bs-ride="carousel">
-        <div className="carousel-inner">
-                <div className="carousel-item active">
-                <img src="/ProjectAssets/Ads/Ad1.jpg" className="d-block w-100" alt="..."/>
-                </div>
-                <div className="carousel-item">
-                <img src="/ProjectAssets/Ads/Ad2.jpg" className="d-block w-100" alt="..."/>
-                </div>
-                <div className="carousel-item">
-                <img src="/ProjectAssets/Ads/Ad3.jpg" className="d-block w-100" alt="..."/>
-                </div>
-                <div className="carousel-item">
-                <img src="/ProjectAssets/Ads/Ad4.jpg" className="d-block w-100" alt="..."/>
-                </div>
-                <div className="carousel-item">
-                <img src="/ProjectAssets/Ads/Ad5.jpg" className="d-block w-100" alt="..."/>
-                </div>
-                <div className="carousel-item">
-                <img src="/ProjectAssets/Ads/Ad6.jpg" className="d-block w-100" alt="..."/>
-                </div>
-        </div>
-
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval10" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval10" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-        </button>  
-    </div>
-    </>
+    <Carousel fade>
+        {
+        imageLibrary.map((e) => {
+            return(
+            <Carousel.Item>
+                <img className="ads-img" src={e.src} alt="" />
+            </Carousel.Item>
+            )
+        })
+        }
+    </Carousel>
   );
 }
 
