5ed3fa6d1fe263b0b2ffff62db3143e0b6f09c28
D3-80669-Apoorva Vairagi <codesaliecc@gmail.com>
2024-02-09 16:40:37 +0530

    Added array for image rendering in Advertisment Carousel

---
+++ React_project/00demo/src/adsImagesArr.js
@@ -0,0 +1,24 @@
+const imageLibrary = [
+    {   id: 1,
+        src: "/ProjectAssets/Ads/Ad1.jpg"
+    },
+
+    {   id: 2,
+        src: "/ProjectAssets/Ads/Ad2.jpg"
+    },
+
+    {   id: 3,
+        src: "/ProjectAssets/Ads/Ad3.jpg"},
+
+    {   id: 4,
+        src: "/ProjectAssets/Ads/Ad4.jpg"
+    },
+
+    {   id: 5, 
+        src: "/ProjectAssets/Ads/Ad5.jpg"},
+        
+    {   id: 6,
+        src: "/ProjectAssets/Ads/Ad6.jpg"},
+];
+
+export default imageLibrary;
