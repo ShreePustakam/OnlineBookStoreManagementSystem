34e2d9ab736bdc48d192380a55c4453827518565
aseem-hub <aseemvairagi@gmail.com>
2024-02-10 20:16:07 +0530

    Added SelfHelpBooks Component

---
+++ React_project/00demo/src/Components/SelfHelpSeries.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import selfHelpBooks from "../selfHelpBooksArr";
+
+function SelfHelpSeries(props) {
+    return <div>
+        <h1>Self Help</h1>
+        <Container>
+        <Row>
+        {
+        selfHelpBooks.map((e) => {
+            return(
+            <Col xs={6} md={4} lg={2}>
+                <Card style={{ width: '10rem'}}>
+                <Card.Img src={e.src} style={{width:100, alignContent:"center" }}/>
+                <Card.Body>
+                    <Card.Title>{e.name}</Card.Title>
+                    <Card.Text>
+                    {e.price}
+                    </Card.Text>
+                    <Button variant="primary">Add</Button>
+                </Card.Body>
+                </Card>
+            </Col>
+            )
+        })
+        }
+      </Row>
+    </Container>
+    </div>
+}
+
+export default SelfHelpSeries;
