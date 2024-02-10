e587cf218252e06aa34fafaf95a98c84ce4cda45
D3-80669-Apoorva Vairagi <codesaliecc@gmail.com>
2024-02-10 20:20:10 +0530

    Manga Book Array added

---
+++ React_project/00demo/src/mangaBooksArr.js
@@ -0,0 +1,34 @@
+const mangaBooks = [
+    {   id: 1,
+        src: "/ProjectAssets/Manga/DBZ.jpg",
+        name: "DBZ",
+        price: "₹120"
+    },
+    {   id: 2,
+        src: "/ProjectAssets/Manga/DemonSlayer.jpg",
+        name: "DemonSlayer",
+        price: "₹239"
+    },
+    {   id: 3,
+        src: "/ProjectAssets/Manga/JJK.jpg",
+        name: "JJK",
+        price: "₹432"
+    },
+    {   id: 4,
+        src: "/ProjectAssets/Manga/kochikame.jpg",
+        name: "Kochikame",
+        price: "₹320"
+    },
+    {   id: 5,
+        src: "/ProjectAssets/Manga/Naruto.jpg",
+        name: "Naruto",
+        price: "₹230"
+    },
+    {   id: 6,
+        src: "/ProjectAssets/Manga/SoyXFamily.jpg",
+        name: "Soy X Family",
+        price: "₹120"
+    },
+]
+
+export default mangaBooks;
