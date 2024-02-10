03aa3c2e62fd2e76063065d7494cfdd7d3570506
aseem-hub <aseemvairagi@gmail.com>
2024-02-10 20:11:10 +0530

    Delete bookset array to modify

---
+++ React_project/00demo/src/selfHelpBooksArr.js
@@ -0,0 +1,34 @@
+const selfHelpBooks = [
+    {   id: 1,
+        src: "/ProjectAssets/SelfHelp/AtomicHabits.jpg",
+        name: "Atomic Habits",
+        price: "₹120"
+    },
+    {   id: 2,
+        src: "/ProjectAssets/SelfHelp/BestYou.jpg",
+        name: "Best You",
+        price: "₹239"
+    },
+    {   id: 3,
+        src: "/ProjectAssets/SelfHelp/The4Agreements.jpg",
+        name: "The four agreements",
+        price: "₹432"
+    },
+    {   id: 4,
+        src: "/ProjectAssets/SelfHelp/TheAlchemist.jpg",
+        name: "The Alchemist",
+        price: "₹320"
+    },
+    {   id: 5,
+        src: "/ProjectAssets/SelfHelp/TheSubtleArtOfNotGivingAFuck.jpg",
+        name: "The Subtle Art of not giving a fuck",
+        price: "₹230"
+    },
+    {   id: 6,
+        src: "/ProjectAssets/SelfHelp/ThinkingFast_Slow.jpg",
+        name: "Thinking Fast and Slow",
+        price: "₹120"
+    },
+]
+
+export default selfHelpBooks;
