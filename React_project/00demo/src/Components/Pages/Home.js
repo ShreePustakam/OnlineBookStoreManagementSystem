621da668726e93d719f476fdfa593079c8eb1052
aseem-hub <aseemvairagi@gmail.com>
2024-02-10 20:00:31 +0530

    Updated Home Page

--- React_project/00demo/src/Components/Pages/Home.js
+++ React_project/00demo/src/Components/Pages/Home.js
@@ -1,25 +1,12 @@
-import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import Footer from '../Footer';
-import '../Styles/home.css';
-import BookSeries from '../BookSeries';
-import Fantasy from '../Fantasy';
-import SelfHelp from '../SelfHelp';
-import Manga from '../Manga';
-import Navbar1 from '../Navbar1';
-import Navbar2 from '../Navbar2';
 import Advertisments from '../Advertisments';
 import BottomAds from '../BottomAds';
-import Book from '../Book';
-import { Router , Switch, Route} from 'react-router-dom';
+import SelfHelpSeries from '../SelfHelpSeries';
 function Home() {
 
    return (
       <>
          <Advertisments/>
-         <BookSeries/>
-         <Fantasy/>
-         <SelfHelp/>
-         <Manga/>
+         <SelfHelpSeries/>
          <BottomAds/>   
       </>
    )
