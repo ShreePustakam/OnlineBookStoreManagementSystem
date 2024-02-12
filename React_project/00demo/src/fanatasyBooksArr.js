9a8c44c615412576650d76f85a39c77f46a92593
D3-80669-Apoorva Vairagi <codesaliecc@gmail.com>
2024-02-12 14:14:38 +0530

    fantasy books array added

---
+++ React_project/00demo/src/fanatasyBooksArr.js
@@ -0,0 +1,29 @@
+const fantasyBooks = [
+    {   id: 1,
+        src: "/ProjectAssets/Fantasy/AllTheBirdsInTheSky.jpg",
+        name: "All The Birds In The Sky",
+        price: "₹120"
+    },
+    {   id: 2,
+        src: "/ProjectAssets/Fantasy/BlackLeopord.jpg",
+        name: "Black Leopord",
+        price: "₹239"
+    },
+    {   id: 3,
+        src: "/ProjectAssets/Fantasy/JadeCity.jpg",
+        name: "Jade City",
+        price: "₹432"
+    },
+    {   id: 4,
+        src: "/ProjectAssets/Fantasy/Legendborn.jpg",
+        name: "Legend Born",
+        price: "₹320"
+    },
+    {   id: 5,
+        src: "/ProjectAssets/Fantasy/TheFifthSeason.jpg",
+        name: "The fifth Season",
+        price: "₹230"
+    },
+]
+
+export default fantasyBooks;
