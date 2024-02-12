7a32f58c7c52c08b31b0e58933942ac196ba348f
D3-80669-Apoorva Vairagi <codesaliecc@gmail.com>
2024-02-12 14:16:07 +0530

    fantasySeries component added

---
+++ React_project/00demo/src/Components/FantasySeries.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import fantasyBooks from "../fanatasyBooksArr";
+
+function FantasySeries(props) {
+    return <div>
+        <h1>Fantasy</h1>
+        <Container>
+        <Row>
+        {
+        fantasyBooks.map((e) => {
+            return(
+            <Col xs={6} md={4} lg={2}>
+                <Card style={{ width: '10rem'}}>
+                <Card.Img src={e.src} style={{width:100, alignContent:"center" }}/>
+                <Card.Body>
+                    <Card.Title>{e.name}</Card.Title>
+                    <Card.Text>
+                    {e.price}
+                    </Card.Text>
+                    <Button variant="primary">Add</Button>
+                </Card.Body>
+                </Card>
+            </Col>
+            )
+        })
+        }
+      </Row>
+    </Container>
+    </div>
+}
+
+export default FantasySeries;
