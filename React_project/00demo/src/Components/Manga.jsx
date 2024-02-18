55d70c55d2421b93f1f650b07902b50bd95ed0f4
aseem-hub <aseemvairagi@gmail.com>
2024-02-18 09:46:33 +0530

    State enum updated

---
+++ React_project/00demo/src/Components/Manga.jsx
@@ -0,0 +1,104 @@
+import './Styles/componentStyles.css'
+
+function Manga(){
+    return(
+        <>
+         <div className='componentDiv' id='manga'>
+            <h2>Manga</h2>
+            <div className="cardHolder">
+                <div className="card">
+                <button type="button" class="btn btn-default btn-heart">
+                    <span class="glyphicon glyphicon-heart"></span>
+                    <i class="fa-sharp fa-regular fa-heart"></i>
+                    </button>
+                    <img src="/ProjectAssets/Manga/DBZ.jpg" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Dragon Ball Z</h5>
+                        <div className='pricing'>
+                        <h6>₹3220</h6>
+                        <h6>⭐⭐</h6>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="card">
+                <button type="button" class="btn btn-default btn-heart">
+                    <span class="glyphicon glyphicon-heart"></span>
+                    <i class="fa-sharp fa-regular fa-heart"></i>
+                    </button>
+                    <img src="/ProjectAssets/Manga/JJK.jpg" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Jujutsu Kaisen</h5>
+                        <div className='pricing'>
+                        <h6>₹3220</h6>
+                        <h6>⭐⭐</h6>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="card">
+                <button type="button" class="btn btn-default btn-heart">
+                    <span class="glyphicon glyphicon-heart"></span>
+                    <i class="fa-sharp fa-regular fa-heart"></i>
+                    </button>
+                    <img src="/ProjectAssets/Manga/DemonSlayer.jpg" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Demon Slayer</h5>
+                        <div className='pricing'>
+                        <h6>₹3220</h6>
+                        <h6>⭐⭐</h6>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="card">
+                <button type="button" class="btn btn-default btn-heart">
+                    <span class="glyphicon glyphicon-heart"></span>
+                    <i class="fa-sharp fa-regular fa-heart"></i>
+                    </button>
+                    <img src="/ProjectAssets/Manga/kochikame.jpg" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Kochikame</h5>
+                        <div className='pricing'>
+                        <h6>₹3220</h6>
+                        <h6>⭐⭐</h6>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="card">
+                <button type="button" class="btn btn-default btn-heart">
+                    <span class="glyphicon glyphicon-heart"></span>
+                    <i class="fa-sharp fa-regular fa-heart"></i>
+                    </button>
+                    <img src="/ProjectAssets/Manga/Naruto.jpg" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Naruto Shippuden</h5>
+                        <div className='pricing'>
+                        <h6>₹3220</h6>
+                        <h6>⭐⭐</h6>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="card">
+                <button type="button" class="btn btn-default btn-heart">
+                    <span class="glyphicon glyphicon-heart"></span>
+                    <i class="fa-sharp fa-regular fa-heart"></i>
+                    </button>
+                    <img src="/ProjectAssets/Manga/SoyXFamily.jpg" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Spy X Family</h5>
+                        <div className='pricing'>
+                        <h6>₹3220</h6>
+                        <h6>⭐⭐</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Manga;
