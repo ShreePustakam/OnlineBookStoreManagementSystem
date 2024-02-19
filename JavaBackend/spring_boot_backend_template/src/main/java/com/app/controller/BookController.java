678a841f96de0deedc1a6a62659c6148ce099f2f
hrutikpiprade <pipradehrutik@gmail.com>
2024-02-19 17:46:31 +0530

    CORS enabled

--- JavaBackend/spring_boot_backend_template/src/main/java/com/app/controller/BookController.java
+++ JavaBackend/spring_boot_backend_template/src/main/java/com/app/controller/BookController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,6 +35,7 @@ import com.app.service.ImageHandlingService;
 @RestController
 @RequestMapping("/books")
 @Validated
+@CrossOrigin(origins = "http://localhost:3000")
 public class BookController {
 	@Autowired
 	private BookService bookService;
--- JavaBackend/spring_boot_backend_template/src/main/java/com/app/controller/BookQtyController.java
+++ JavaBackend/spring_boot_backend_template/src/main/java/com/app/controller/BookQtyController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 @RestController
 @RequestMapping("/bookQty")
 @Validated
+@CrossOrigin(origins = "http://localhost:3000")
 public class BookQtyController {
 
 	@Autowired
