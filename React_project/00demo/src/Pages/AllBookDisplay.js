6738ad6e84e3ce19f8788cb848e37a9f82214668
hrutikpiprade <pipradehrutik@gmail.com>
2024-02-21 23:21:54 +0530

    AllBookDisplay page added

---
+++ React_project/00demo/src/Pages/AllBookDisplay.js
@@ -0,0 +1,16 @@
+import Navbar2 from '../Components/Navbar2';
+import Footer from '../Components/Footer';
+import AllBooks from '../Components/AllBooks';
+
+function AllBookDisplay() {
+
+   return (
+      <>
+         <Navbar2/>
+         <AllBooks/> 
+         <Footer/>
+      </>
+   )
+}
+
+export default AllBookDisplay;
