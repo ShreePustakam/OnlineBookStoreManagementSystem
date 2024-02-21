c76fc6d52ef93e62957cc0dc2ba1e18de5b32f82
hrutikpiprade <pipradehrutik@gmail.com>
2024-02-21 23:31:07 +0530

    bookservice updated in react

--- React_project/00demo/src/Services/book.service.js
+++ React_project/00demo/src/Services/book.service.js
@@ -3,5 +3,14 @@ import httpCLient from "../http-common";
 const postAddBookInfo = (data) =>{
     return httpCLient.post('books/add', data);
 }
+const allBookInfo = (data) =>{
+    return httpCLient.get('books/');
+}
+const updateBookInfo=(data)=>{
+    return httpCLient.put('/books',data);
+}
+const getBook=(isbn)=>{
+    return httpCLient.get('/books/'+isbn);
+}
 
-export default {postAddBookInfo};
+export default {postAddBookInfo,allBookInfo,updateBookInfo,getBook };
