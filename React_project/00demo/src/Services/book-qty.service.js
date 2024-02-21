6b4d3c7093569482d383634dea7e6a774760e255
KunalP1303 <kunalpatle.kp@gmail.com>
2024-02-22 04:23:51 +0530

    Cart service function added

--- React_project/00demo/src/Services/book-qty.service.js
+++ React_project/00demo/src/Services/book-qty.service.js
@@ -4,4 +4,16 @@ const addToCart = (cId, isbn) => {
     return httpClient.post('/bookQty/cart/' + cId + '/' + isbn);
 }
 
-export default { addToCart };
+const displayCart = (cId) => {
+    return httpClient.get('bookQty/cart/' + cId);
+}
+
+const setBookQuantity = (data) => {
+    return httpClient.put('bookQty/cart/qty', data)
+}
+
+const removeBook = (cId, isbn) => {
+    return httpClient.delete('/bookQty/cart/' + cId + '/' + isbn);
+}
+
+export default { addToCart, displayCart, setBookQuantity, removeBookA };
