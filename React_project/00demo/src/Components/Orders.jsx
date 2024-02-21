d53cc06660c50ddc974055361191b987fce08941
hrutikpiprade <pipradehrutik@gmail.com>
2024-02-21 12:32:02 +0530

    order component added

--- React_project/00demo/src/Components/Orders.jsx
+++ React_project/00demo/src/Components/Orders.jsx
@@ -1,40 +1,59 @@
-import React from "react";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import '../Styles/cartStyle.css';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Table, Container } from 'react-bootstrap';
+import axios from 'axios';
+import { myOrders } from '../Services/order.service';
 
-function Orders() {
-    return <Container className='yourCart'>
-    <h1>Your Orders</h1>
-    <Row className='cart-rows'>
-      <Col xs={6} md={4} lg={2}>
-      <img src="/ProjectAssets/SelfHelp/AtomicHabits.jpg" alt="" />
-      </Col>
+// ... (import statements)
 
-      <Col xs={6} md={4} lg={2}>
-      <h3>Book Title</h3>
-      </Col>
+const OrderPage = () => {
+  const [orders, setOrders] = useState([]);
+  const cId = sessionStorage.getItem('cId');
 
-      <Col xs={6} md={4} lg={2}>
-      <h5>Order id:</h5>
-      </Col>
+  useEffect(() => {
+    // Fetch orders when the component mounts
+    fetchOrders();
+  }, [cId]);
 
-      <Col xs={6} md={4} lg={2}>
-      <h5>Delivery Date</h5>
-      </Col>
+  const fetchOrders = async () => {
+    const fetchedOrders = await myOrders(cId);
+    setOrders(fetchedOrders || []); // Ensure that fetchedOrders is not undefined
+  };
 
-      <Col xs={6} md={4} lg={2}>
-      <h5>Qty: 08</h5>
-      </Col>
+  return (
+    <Container className="my-5" style={{ background: 'white' }}>
+      <h1>My Orders</h1>
+      <Table responsive bordered hover>
+        <thead>
+          <tr>
+            <th>Order ID</th>
+            <th>Total Amount (₹)</th>
+            <th>Status</th>
+            <th>Order Date</th>
+            <th>Delivery Date</th>
+            <th>Action</th> {/* Add a new column for the "View Bill" button */}
+          </tr>
+        </thead>
+        <tbody>
+          {orders && orders.map(order => (
+            <tr key={order.orderId}>
+              <td>{order.orderId}</td>
+              <td>{order.totalAmount}</td>
+              <td>{order.ostatus}</td>
+              <td>{order.odate}</td>
+              <td>{order.ddate}</td>
+              <td>
+                <Link to={`/bill?oId=${order.orderId}`}>
+                  <button className="btn btn-primary">View Bill</button>
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
+};
 
-      <Col xs={6} md={4} lg={2}>
-      <h5>rs. 600</h5>
-      </Col>
+export default OrderPage;
 
-    </Row>
-    
-  </Container>
-}
-
-export default Orders;
