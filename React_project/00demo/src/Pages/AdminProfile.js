93a6f21c44bd338abdad0e121d3cbc2d49c7554b
D3-80669-Apoorva Vairagi <codesaliecc@gmail.com>
2024-02-22 12:55:49 +0530

    admin logout added

--- React_project/00demo/src/Pages/AdminProfile.js
+++ React_project/00demo/src/Pages/AdminProfile.js
@@ -1,54 +1,67 @@
 import { Link } from "react-router-dom/cjs/react-router-dom";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import "../Styles/userProfileStyle.css";
 
 function AdminProfile() {
+
+    const history = useHistory();
+
+    function logout() {
+        sessionStorage.clear();
+        history.push("/home");
+    }
+
     return <Container className="user-container">
-            <Row className="user-profile">
+        <Row className="user-profile">
 
-                <Col>
-                    <img src="ProjectAssets\signin-image.jpg"></img>
-                </Col>
+            <Col>
+                <img src="ProjectAssets\signin-image.jpg"></img>
+            </Col>
 
-                <Col>
-                    <h1 className='logo-font'><a href="/home" className='logo-font'>Pustakam Admin</a></h1>
+            <Col>
+                <h1 className='logo-font'><a href="/home" className='logo-font'>Pustakam Admin</a></h1>
 
-                    <div className='user-info'>
-                        <h3>{sessionStorage.getItem("userName")}</h3>
-                        <h5>{sessionStorage.getItem("email")}</h5>
-                        <h5>{sessionStorage.getItem("phoneNo")}</h5>
+                <div className='user-info'>
+                    <h3>{sessionStorage.getItem("userName")}</h3>
+                    <h5>{sessionStorage.getItem("email")}</h5>
+                    <h5>{sessionStorage.getItem("phoneNo")}</h5>
 
-                        <div className='your-options' style={{backgroundColor:"#FFF6F6"}}>
-                            <div>
-                                <a href="#" class="user-link">
-                                    <Link to="/allorders">All Orders </Link>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-basket-fill" viewBox="0 0 16 16">
-                                        <path d="M5.071 1.243a.5.5 0 0 1 .858.514L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 6h1.717zM3.5 10.5a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0z"></path>
-                                    </svg>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="#" class="user-link">
-                                    <Link to="/allbooks">All Books </Link>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-basket-fill" viewBox="0 0 16 16">
-                                        <path d="M5.071 1.243a.5.5 0 0 1 .858.514L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 6h1.717zM3.5 10.5a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0z"></path>
-                                    </svg>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="#" class="user-link">
-                                    <Link to="/addbook">Add Book </Link>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-basket-fill" viewBox="0 0 16 16">
-                                        <path d="M5.071 1.243a.5.5 0 0 1 .858.514L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 6h1.717zM3.5 10.5a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0z"></path>
-                                    </svg>
-                                </a>
-                            </div>
+                    <div className='your-options' style={{ backgroundColor: "#FFF6F6" }}>
+                        <div>
+                            <a href="#" class="user-link">
+                                <Link to="/allorders">All Orders </Link>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-basket-fill" viewBox="0 0 16 16">
+                                    <path d="M5.071 1.243a.5.5 0 0 1 .858.514L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 6h1.717zM3.5 10.5a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0z"></path>
+                                </svg>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="#" class="user-link">
+                                <Link to="/allbooks">All Books </Link>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-basket-fill" viewBox="0 0 16 16">
+                                    <path d="M5.071 1.243a.5.5 0 0 1 .858.514L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 6h1.717zM3.5 10.5a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0z"></path>
+                                </svg>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="#" class="user-link">
+                                <Link to="/addbook">Add Book </Link>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-basket-fill" viewBox="0 0 16 16">
+                                    <path d="M5.071 1.243a.5.5 0 0 1 .858.514L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 6h1.717zM3.5 10.5a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0z"></path>
+                                </svg>
+                            </a>
                         </div>
                     </div>
-                </Col>
-            </Row>
+                </div>
+                <a href="#" class="user-link" onClick={logout} style={{ marginTop: 20, marginLeft: 30 }}>
+                    <Link to="/editprofile">Logout </Link>
+                </a>
+            </Col>
+        </Row>
     </Container>
 }
 
